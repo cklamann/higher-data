aaf46f78020966d803af3e03a5fddbd0e257bde1
Conor Klamann <cklamann@culturaldata.org>
2018-01-25 13:53:29 -0500

    add chart-search to shared module, chart page

--- client/src/app/modules/shared/shared.module.ts
+++ client/src/app/modules/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SchoolSearchComponent } from './school-search/school-search.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule, MatFormFieldModule, MatOptionModule, MatInputModule } from '@angular/material';
+import { ChartSearchComponent } from './chart-search/chart-search.component';
 
 @NgModule({
 	imports: [
 		CommonModule, ReactiveFormsModule, MatAutocompleteModule, MatFormFieldModule, MatOptionModule, MatInputModule
 	],
-	declarations: [SchoolSearchComponent],
+	declarations: [SchoolSearchComponent, ChartSearchComponent],
 	exports: [
-		SchoolSearchComponent
+		SchoolSearchComponent, ChartSearchComponent
 	]
 })
 export class SharedModule { }
