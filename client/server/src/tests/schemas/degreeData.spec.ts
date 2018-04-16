e9f179aa68afb9e11bfb9fc0ce586845230a6368
Conor Klamann <cklamann@culturaldata.org>
2018-04-16 13:11:06 -0400

    remove degree data test

--- client/server/src/tests/schemas/degreeData.spec.ts
+++ client/server/src/tests/schemas/degreeData.spec.ts
@@ -4,19 +4,19 @@ import chai = require('chai');
 import { expect } from 'chai';
 const app = require('../../app');
 
-describe('CipMap Schema', function() {
+describe('DegreeData Schema', function() {
 
-  describe('#fetch()', function() {
-    it('should fetch the schema', function(done) {
-      DegreeDataSchema.find().limit(50)
-        .then(schema => {
-          expect(schema).to.be.an('array');
-          expect(schema[0]).to.have.property('fiscal_year');
-          expect(schema[0]).to.have.property('awlevel');
-          done();
-        })
-        .catch(err => done(err));
-    });
-  });
+  // describe('#fetch()', function() {
+  //   it('should fetch the schema', function(done) {
+  //     DegreeDataSchema.find().limit(50)
+  //       .then(schema => {
+  //         expect(schema).to.be.an('array');
+  //         expect(schema[0]).to.have.property('fiscal_year');
+  //         expect(schema[0]).to.have.property('awlevel');
+  //         done();
+  //       })
+  //       .catch(err => done(err));
+  //   });
+  // });
 
 });
