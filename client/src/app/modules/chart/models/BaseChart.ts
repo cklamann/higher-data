5e644d09b4122c13ce70ac1ad9e94a35b7c8636a
conor <conorklamann@gmail.com>
2018-08-24 10:41:50 -0400

    fix chart width issue

--- client/src/app/modules/chart/models/BaseChart.ts
+++ client/src/app/modules/chart/models/BaseChart.ts
@@ -67,9 +67,8 @@ export class BaseChart {
 	buildCanvas() {
 		this.container = d3.select("." + this.selector);
 		const winWidth = window.innerWidth;
-		//angular material: layout-gt-xs = width >= 600px; layout-gt-sm =	width >= 960px
-		//.52 is width of chart canvas on full size, one smaller size, just leave 25px gutter
-		let w = winWidth > 960 ? .52 * winWidth : winWidth > 600 ? winWidth - 50 : winWidth - 20;
+		//angular material: layout-gt-sm >= 960px
+		let w = winWidth > 1500 ? 825 : winWidth > 960 ? (winWidth - 50) * .52 : winWidth - 20;
 		w = w * this.displayOptions.widthRatio;
 		this.width = w - this.displayOptions.margins.left - this.displayOptions.margins.right;
 		this.height = (this.width / 1.5) - this.displayOptions.margins.top - this.displayOptions.margins.bottom;
