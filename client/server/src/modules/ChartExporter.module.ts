7876d0c51a22ec4cb116500356c833a81d77c83b
conor <conorklamann@gmail.com>
2018-10-20 21:38:21 -0400

    add null check to inflation adjuster

--- client/server/src/modules/ChartExporter.module.ts
+++ client/server/src/modules/ChartExporter.module.ts
@@ -69,8 +69,13 @@ export class ChartExport {
 	}
 	_adjustForInflation(res: intFormulaParserResult[]): Promise<intFormulaParserResult[]> {
 		return getInflationAdjuster().then(adjuster => {
-			res.forEach(item => item.value = adjuster(item.fiscal_year, item.value));
-			return res;
+			return res.map(item => { 
+				if(item.value){
+					item.value = adjuster(item.fiscal_year, item.value);
+				}
+				//did we not drop NAs from wages? No. So what's going on?
+				return item;
+			});
 		});
 	}
 }
